03bdbc2c67679da7b7b9c2bf8381e012bb14d00a
Andrejus <andrejus@think-engineer.com>
2018-04-02 00:16:46 +0300

    Add vector-based thrusting to player movement

--- app/src/main/java/uk/ac/reading/student/akostarevas/asteroids/Player.java
+++ app/src/main/java/uk/ac/reading/student/akostarevas/asteroids/Player.java
@@ -1,29 +1,62 @@
 package uk.ac.reading.student.akostarevas.asteroids;
 
 import android.graphics.Canvas;
-import android.view.KeyEvent;
 
 @SuppressWarnings("unused")
 class Player extends MovableObject {
 
     private final static float angleMultiplier = 100;
+    private final static float thrustSpeed = 5;
+
+    private float thrustAngle;
 
     boolean turningLeft;
     boolean turningRight;
+    boolean thrusting;
 
     Player(int canvasWidth, int canvasHeight) {
         super(canvasWidth / 2, canvasHeight / 2, canvasWidth, canvasHeight);
+
         turningLeft = false;
         turningRight = false;
+
+        thrustAngle = 0;
+        thrusting = false;
     }
 
     void move(float secondsElapsed) {
+
+        /* Turning */
         if (turningLeft) {
-            this.angle -= secondsElapsed * angleMultiplier;
+            this.thrustAngle -= secondsElapsed * angleMultiplier;
         }
         if (turningRight) {
-            this.angle += secondsElapsed * angleMultiplier;
+            this.thrustAngle += secondsElapsed * angleMultiplier;
         }
+
+        /* Movement */
+        if (thrusting) {
+
+            /* Cross product vectors */
+            double movementAngleRadians = (angle / 180.0 * Math.PI);
+            double xMove = (velocity) * Math.sin(movementAngleRadians);
+            double yMove = (velocity) * Math.cos(movementAngleRadians);
+
+            double thrustAngleRadians = (thrustAngle / 180.0 * Math.PI);
+            double xThrust = (thrustSpeed) * Math.sin(thrustAngleRadians);
+            double yThrust = (thrustSpeed) * Math.cos(thrustAngleRadians);
+
+            double xFinal = xMove + xThrust;
+            double yFinal = yMove + yThrust;
+
+            /* Sum of squares */
+            velocity = (float) Math.sqrt(Math.pow(xFinal, 2) + Math.pow(yFinal, 2));
+
+            /* Quick maths */
+            angle = (float) Math.atan(xFinal / yFinal);
+            
+        }
+
         super.move(secondsElapsed);
     }
 
