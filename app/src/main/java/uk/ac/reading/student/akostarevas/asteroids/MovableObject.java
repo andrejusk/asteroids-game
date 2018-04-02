06a72bac0e2c627225c5f1360c49abb5b3de0b34
Andrejus <andrejus@think-engineer.com>
2018-04-02 20:28:26 +0300

    Add special flags for object bound tracking

--- app/src/main/java/uk/ac/reading/student/akostarevas/asteroids/MovableObject.java
+++ app/src/main/java/uk/ac/reading/student/akostarevas/asteroids/MovableObject.java
@@ -4,10 +4,12 @@ import android.graphics.Canvas;
 
 class MovableObject extends StaticObject {
 
+    /* Physics variables */
     final static int tailMultiplier = 10;
     private final static int speedMultiplier = 100;
-    private final static int dropoffMultiplier = 2;
+    private final static int dropOffMultiplier = 2;
 
+    /* Keep track of canvas */
     private int canvasWidth;
     private int canvasHeight;
 
@@ -15,32 +17,71 @@ class MovableObject extends StaticObject {
     float angle;
     float velocity;
 
+    /* Special flags */
+    boolean warp;
+    boolean enteringBounds;
+    boolean exitedBounds = false;
+
     MovableObject(float x, float y, int canvasWidth, int canvasHeight) {
         this(x, y, canvasWidth, canvasHeight, 0, 0);
     }
 
     MovableObject(float x, float y, int canvasWidth, int canvasHeight, float angle, float velocity) {
+        this(x, y, canvasWidth, canvasHeight, angle, velocity, false);
+    }
+
+    MovableObject(float x, float y, int canvasWidth, int canvasHeight, float angle, float velocity, boolean enteringBounds) {
         super(x, y);
         this.canvasWidth = canvasWidth;
         this.canvasHeight = canvasHeight;
         this.angle = angle;
         this.velocity = velocity;
+        this.enteringBounds = enteringBounds;
+        this.warp = !enteringBounds;
     }
 
     void move(float secondsElapsed) {
-        /* Update velocity */
-        velocity -= Math.log(velocity + 1) * secondsElapsed / dropoffMultiplier;
+        /* Update velocity with drop off */
+        velocity -= Math.log(velocity + 1) * secondsElapsed / dropOffMultiplier;
 
+        /* Calculate displacement */
         double angleRadians = (angle / 180.0 * Math.PI);
+        double speed = velocity * speedMultiplier;
+        double xDisplace = speed * Math.sin(angleRadians) * secondsElapsed;
+        double yDisplace = speed * Math.cos(angleRadians) * secondsElapsed;
 
-        double xSpeed = (velocity * speedMultiplier) * Math.sin(angleRadians);
-        double ySpeed = (velocity * speedMultiplier) * Math.cos(angleRadians);
+        /* Apply displacement */
+        double xRaw = x + xDisplace;
+        double yRaw = y + yDisplace;
 
-        float xRaw = (float) ((x + secondsElapsed * xSpeed) % canvasWidth);
-        float yRaw = (float) ((y + secondsElapsed * ySpeed) % canvasHeight);
+        /* If out of bounds */
+        if (xRaw > canvasWidth || xRaw < 0 || yRaw > canvasHeight || yRaw < 0) {
+            /* If not supposed to be out of bounds */
+            if (!enteringBounds) {
+                exitedBounds = true;
+            }
+        }
 
-        x = (xRaw < 0) ? xRaw + canvasWidth : xRaw;
-        y = (yRaw < 0) ? yRaw + canvasHeight : yRaw;
+        /* If within bounds */
+        if (xRaw <= canvasWidth && xRaw >= 0 && yRaw <= canvasHeight && yRaw >= 0) {
+            /* If started out of bounds */
+            if (enteringBounds) {
+                enteringBounds = false;
+            }
+        }
+
+        if (warp) {
+            /* Warp bottom and right */
+            xRaw = xRaw % canvasWidth;
+            yRaw = yRaw % canvasHeight;
+
+            /* Warp top and left */
+            xRaw = (xRaw < 0) ? xRaw + canvasWidth : xRaw;
+            yRaw = (yRaw < 0) ? yRaw + canvasHeight : yRaw;
+        }
+
+        x = (float) xRaw;
+        y = (float) yRaw;
     }
 
     @Override
